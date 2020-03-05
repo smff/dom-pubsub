b8dce5df85d3835a7ae5e6bb28b6672f59fe06f3
Danil Kamyshov <dkamyshov@Danils-MacBook-Pro.local>
2020-03-05 18:51:22 +0300

    refactor: fix issues, change build pipeline
    - add option to cancel a subscription
    - check for window presence
    - check for document.body presence
    - get rid of babel
    - add prettier
    - generate d.ts.map-file
    - remove unnecessary stuff from tsconfig.json
    - refactor .npmignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,28 @@
 var path = require('path');
 
 module.exports = {
-    entry: './src/index',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app.bundle.js'
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json']
-    },
-    module: {
-        rules: [{
-            test: /\.(ts|js)x?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        }],
-    }
+  mode: 'production',
+  entry: './src/index',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+    library: 'DOMPubSub',
+    libraryTarget: 'commonjs2'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'awesome-typescript-loader',
+          options: {
+            transpileOnly: true
+          }
+        }
+      }
+    ]
+  }
 };
